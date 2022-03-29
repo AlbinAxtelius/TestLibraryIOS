2f2eda89a7fc594cab1c46589eaafe0002a3203b
Albin Axtelius <albin@axtelius.se>
2022-03-29 09:01:54 +0200

    Initial commit

--- Sources/BleManagerIOS/BleManagerIOS.swift
+++ Sources/BleManagerIOS/BleManagerIOS.swift
@@ -3,4 +3,13 @@ public struct BleManagerIOS {
 
     public init() {
     }
+	
+	private var coolTemplate: String = "{NAME} is very cool"
+	
+	public func getCoolWithName(name: String) -> String {
+		return coolTemplate.split(separator: " ").map({ subString -> Substring in
+			if subString == "{NAME}" { return Substring(name) }
+			return subString
+		}).joined(separator: " ")
+	}
 }
--- Tests/BleManagerIOSTests/BleManagerIOSTests.swift
+++ Tests/BleManagerIOSTests/BleManagerIOSTests.swift
@@ -2,10 +2,11 @@ import XCTest
 @testable import BleManagerIOS
 
 final class BleManagerIOSTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(BleManagerIOS().text, "Hello, World!")
-    }
+	func testExample() throws {
+		XCTAssertEqual(BleManagerIOS().text, "Hello, World!")
+	}
+	
+	func testCoolText() throws {
+		XCTAssertEqual(BleManagerIOS().getCoolWithName(name: "Albin"), "Albin is very cool")
+	}
 }
