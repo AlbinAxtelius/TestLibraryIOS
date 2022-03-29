5a74726fd9474d1261f6e09c6f9bc124fe58bec9
Albin Axtelius <albin@axtelius.se>
2022-03-29 10:52:32 +0200

    Fixed sources

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 		// Products define the executables and libraries a package produces, and make them visible to other packages.
 		.library(
 			name: "BleManagerIOS",
-			targets: ["BleManagerIOS"])
+			targets: ["BleManagerIOS", "BleManagerButClass"])
 	],
 	dependencies: [
 		.package(
